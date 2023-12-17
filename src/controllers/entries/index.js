555f9fb76e713ca48b30fce9b39a96281f89dd95
gonsevi80 <142497836+gonsevi80@users.noreply.github.com>
2023-12-17 17:34:40 +0100

    Delete src/controllers/entries directory

--- src/controllers/entries/index.js
+++
@@ -1,9 +0,0 @@
-import newEntryController from './newEntryController.js';
-import listEntriesController from './listEntriesController.js';
-import getEntryController from './getEntryController.js';
-
-export {
-    newEntryController,
-    listEntriesController,
-    getEntryController
-};
